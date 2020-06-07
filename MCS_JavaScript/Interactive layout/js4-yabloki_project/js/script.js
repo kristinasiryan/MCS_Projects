037c251909fb71e293e710fd7954f1f89443f297
kristinasiryan <kristinaasiryan@gmail.com>
2020-06-07 22:49:42 +0300

    yabloki_project

--- MCS_JavaScript/Interactive layout/js4-yabloki_project/js/script.js
+++ MCS_JavaScript/Interactive layout/js4-yabloki_project/js/script.js
@@ -1,17 +1,17 @@
 
 const body=document.body;
 
-	d=prompt('Сколько у вас денег?');
-	y=prompt('Сколько вы купили яблок?');
-	x=prompt('Сколько вы купили батонов хлеба?');
-	s=prompt('Сколько стоит одно яблоко?');
-	k=prompt('Сколько стоит один батон хлеба?');
-	m=parseInt(y)*parseInt(s);
-	t=parseInt(x)*parseInt(k);
-	f=parseInt(d)>parseInt(m)+parseInt(t);
+	let d=prompt('Сколько у вас денег?');
+	let y=prompt('Сколько вы купили яблок?');
+	let x=prompt('Сколько вы купили батонов хлеба?');
+	let s=prompt('Сколько стоит одно яблоко?');
+	let k=prompt('Сколько стоит один батон хлеба?');
+	let m=parseInt(y)*parseInt(s);
+	let t=parseInt(x)*parseInt(k);
+	let f=parseInt(d)>parseInt(m)+parseInt(t);
 
-function haveEnough(d, y, x, s, k, m, t, f) {
-	if(f==true) {
+function haveEnough(f) {
+	if(f===true) {
 	body.innerHTML='Вам хватает денег на покупки';
 	console.log('Вам хватает денег на покупки');
 	} else {
@@ -20,4 +20,4 @@ function haveEnough(d, y, x, s, k, m, t, f) {
 	};
 }
 
-haveEnough(d, y, x, s, k, m, t, f);
+haveEnough(f);
